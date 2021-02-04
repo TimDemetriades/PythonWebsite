505b168f0cfbf853f20ec8177baea0023ef91cf4
TimDemetriades <tim.demetriades@gmail.com>
2021-02-04 12:44:11 -0500

    Setup SQLAlchemy for db

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'random string'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'    # store db in website folder
+    db.init_app(app)    # initialize db by giving it the flask app
     
     from .views import views
     from .auth import auth
