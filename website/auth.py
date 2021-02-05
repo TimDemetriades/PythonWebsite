5c3ddbbce7bc21af9f04d5e4f5b15ad35b3005e4
TimDemetriades <tim.demetriades@gmail.com>
2021-02-04 23:00:27 -0500

    Setup user account creation

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User    # for adding user to db
+from werkzeug.security import generate_password_hash, check_password_hash    # for hashing password
+from . import db    # import from current package (folder) the db object
 
 auth = Blueprint('auth', __name__)    # blueprint for flask application
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing")
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -15,13 +18,13 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password = request.form.get('password')
         passwordConfirm = request.form.get('passwordConfirm')
         
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password != passwordConfirm:
             flash('Passwords must match.', category='error')
@@ -29,6 +32,11 @@ def sign_up():
             flash('Password must be longer than 6', category='error')
         else:
             # add user to db
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))    # views is blueprint, home is function within that blueprint (which contains the url)
+            
     return render_template("sign_up.html")
 
